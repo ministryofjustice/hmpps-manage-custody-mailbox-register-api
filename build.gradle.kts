9a92fe3343958771e31438d9b63dbd801ebde7b3
Ryan Kendall <ryankendallt@gmail.com>
2025-01-03 11:35:41 +0000

    added db datasource config

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
+
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.1.0")
   testImplementation("org.wiremock:wiremock-standalone:3.9.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24") {
