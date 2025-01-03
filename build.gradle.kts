992b9e34c7fee2533e47235cc6e66bf449a03fff
Ryan Kendall <ryankendallt@gmail.com>
2025-01-03 11:39:28 +0000

    added jpa dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
   runtimeOnly("org.postgresql:postgresql:42.7.3")
 
