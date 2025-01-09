77c7b5a1b4d17b4a4f4344689aaeb58fd5b5f259
Ryan Kendall <ryankendallt@gmail.com>
2025-01-09 15:26:57 +0000

    Add JPA and Flyway (#6)
    * updated banner text
    
    * added db datasource config
    
    * added jpa dependency
    
    * added flyway
    
    * Added h2 as test db
    
    * remove duplication in config

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,13 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.1.0")
+  testImplementation("com.h2database:h2")
   testImplementation("org.wiremock:wiremock-standalone:3.9.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24") {
     exclude(group = "io.swagger.core.v3")
