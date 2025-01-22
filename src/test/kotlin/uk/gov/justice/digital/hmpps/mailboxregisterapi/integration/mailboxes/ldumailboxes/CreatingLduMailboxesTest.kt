0cc7383ba68018b556a64534008413a7deca12e4
Ryan Kendall <ryankendallt@gmail.com>
2025-01-22 10:48:11 +0000

    Unnested tests from inner class to package

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/mailboxregisterapi/integration/mailboxes/ldumailboxes/CreatingLduMailboxesTest.kt
@@ -0,0 +1,105 @@
+package uk.gov.justice.digital.hmpps.mailboxregisterapi.integration.mailboxes.ldumailboxes
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.mailboxregisterapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.mailboxregisterapi.mailboxes.LocalDeliveryUnitMailboxRepository
+
+private const val BASE_URI: String = "/local-delivery-unit-mailboxes"
+
+@DisplayName("POST /local-delivery-unit-mailboxes")
+class CreatingLduMailboxesTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var localDeliveryUnitMailboxes: LocalDeliveryUnitMailboxRepository
+
+  private lateinit var attributes: HashMap<String, String?>
+
+  @BeforeEach
+  fun setup() {
+    attributes = HashMap<String, String?>().apply {
+      put("unitCode", "UNIT_CODE")
+      put("areaCode", "AREA_CODE")
+      put("name", "Mailbox Name")
+      put("emailAddress", "ldu@example.com")
+      put("country", "England")
+    }
+  }
+
+  @Test
+  fun `should return unauthorized if no token`() {
+    webTestClient.post()
+      .uri(BASE_URI)
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+
+  @Test
+  fun `should return forbidden if no role`() {
+    webTestClient.post()
+      .uri(BASE_URI)
+      .headers(setAuthorisation())
+      .bodyValue(attributes)
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `should return forbidden if wrong role`() {
+    webTestClient.post()
+      .uri(BASE_URI)
+      .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+      .bodyValue(attributes)
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `are created by submitting the correct details`() {
+    webTestClient.post()
+      .uri(BASE_URI)
+      .headers(setAuthorisation(roles = listOf("MAILBOX_REGISTER_ADMIN")))
+      .bodyValue(attributes)
+      .exchange()
+      .expectStatus().isCreated
+
+    Assertions.assertThat(localDeliveryUnitMailboxes.count()).isOne()
+
+    localDeliveryUnitMailboxes.findAll().first().apply {
+      Assertions.assertThat(unitCode).isEqualTo("UNIT_CODE")
+      Assertions.assertThat(areaCode).isEqualTo("AREA_CODE")
+      Assertions.assertThat(name).isEqualTo("Mailbox Name")
+      Assertions.assertThat(emailAddress).isEqualTo("ldu@example.com")
+      Assertions.assertThat(country).isEqualTo("England")
+    }
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["unitCode", "areaCode", "emailAddress"])
+  fun `without the required fields mailboxes are not created`(nullFieldName: String) {
+    val invalidAttributes = attributes.toMutableMap().apply {
+      this[nullFieldName] = null
+    }
+
+    webTestClient.post()
+      .uri(BASE_URI)
+      .headers(setAuthorisation(roles = listOf("MAILBOX_REGISTER_ADMIN")))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(invalidAttributes)
+      .exchange()
+      .expectStatus().isBadRequest
+      .expectBody().jsonPath("$.errors.$nullFieldName").isEqualTo("must not be blank")
+
+    Assertions.assertThat(localDeliveryUnitMailboxes.count()).isZero
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/mailboxregisterapi/integration/mailboxes/ldumailboxes/GettingLduMailboxesTest.kt
@@ -0,0 +1,90 @@
+package uk.gov.justice.digital.hmpps.mailboxregisterapi.integration.mailboxes.ldumailboxes
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.test.context.jdbc.Sql
+import uk.gov.justice.digital.hmpps.mailboxregisterapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.mailboxregisterapi.mailboxes.LocalDeliveryUnitMailbox
+import uk.gov.justice.digital.hmpps.mailboxregisterapi.mailboxes.LocalDeliveryUnitMailboxRepository
+
+private const val DUMMY_MAILBOX_ID = "8d044b2e-96b1-45ef-a2ce-cce9c6f6a0c2"
+private const val BASE_URI: String = "/local-delivery-unit-mailboxes"
+
+@DisplayName("GET /local-delivery-unit-mailboxes/:id")
+class GettingLduMailboxesTest : IntegrationTestBase() {
+
+  @Autowired
+  lateinit var localDeliveryUnitMailboxes: LocalDeliveryUnitMailboxRepository
+
+  @Test
+  fun `should return unauthorized if no token`() {
+    webTestClient.get()
+      .uri("$BASE_URI/$DUMMY_MAILBOX_ID")
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+
+  @Test
+  fun `should return forbidden if no role`() {
+    webTestClient.get()
+      .uri("$BASE_URI/$DUMMY_MAILBOX_ID")
+      .headers(setAuthorisation())
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `should return forbidden if wrong role`() {
+    webTestClient.get()
+      .uri("$BASE_URI/$DUMMY_MAILBOX_ID")
+      .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `should return not found if mailbox does not exist`() {
+    webTestClient.get()
+      .uri("$BASE_URI/$DUMMY_MAILBOX_ID")
+      .headers(setAuthorisation(roles = listOf("MAILBOX_REGISTER_ADMIN")))
+      .exchange()
+      .expectStatus()
+      .isNotFound
+  }
+
+  @Sql(
+    "classpath:test_data/reset.sql",
+    "classpath:test_data/some_ldu_mailboxes.sql",
+  )
+  @Test
+  fun `should return the mailbox details if it exists`() {
+    val mailboxId = localDeliveryUnitMailboxes.findAll().first().id
+    Assertions.assertThat(mailboxId).isNotNull
+
+    val mailbox = webTestClient.get()
+      .uri("$BASE_URI/$mailboxId")
+      .headers(setAuthorisation(roles = listOf("MAILBOX_REGISTER_ADMIN")))
+      .exchange()
+      .expectStatus()
+      .isOk.expectBody(object : ParameterizedTypeReference<LocalDeliveryUnitMailbox>() {})
+      .returnResult().responseBody!!
+
+    mailbox.apply {
+      Assertions.assertThat(id).isEqualTo(mailboxId)
+      Assertions.assertThat(unitCode).isEqualTo("UNIT_CODE_1")
+      Assertions.assertThat(areaCode).isEqualTo("AREA_CODE_1")
+      Assertions.assertThat(name).isEqualTo("Test Mailbox 1")
+      Assertions.assertThat(emailAddress).isEqualTo("ldu1@example.com")
+      Assertions.assertThat(country).isEqualTo("England")
+      Assertions.assertThat(createdAt).isNotNull
+      Assertions.assertThat(updatedAt).isNotNull
+    }
+  }
+}
